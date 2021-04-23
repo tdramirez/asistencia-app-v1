97763472e26b6c057474d08e39cdcaf852f1dac7
tdramirez <tdramirez@est.itsgg.edu.ec>
2021-04-22 21:41:10 -0500

    tercer cambio an falta

--- app/src/main/java/com/example/myasistencia/ListaEstudiantes.java
+++ app/src/main/java/com/example/myasistencia/ListaEstudiantes.java
@@ -3,12 +3,23 @@ package com.example.myasistencia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+
+
 
 public class ListaEstudiantes extends AppCompatActivity {
+
+    private TextView text1;
+    Bundle nombreCuro = (Bundle)getIntent().getExtras();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_estudiantes);
+
+        text1 = (TextView)findViewById(R.id.titulo);
+        text1.setText("Curso "+nombreCuro.getString("parametro"));
+
     }
 }
--- app/src/main/java/com/example/myasistencia/MainActivity.java
+++ app/src/main/java/com/example/myasistencia/MainActivity.java
@@ -8,21 +8,23 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.content.Intent;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private Spinner spinner1 ;
     private Button boton1;
 
-    String[] strStudianes;
-    List<String> listaEstdiante;
+    String[] strCursos;
+    List<String> listaCursos;
     ArrayAdapter<String> comboAdapter;
+    String nombreCurso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +33,13 @@ public class MainActivity extends AppCompatActivity {
 
         spinner1 = (Spinner)findViewById(R.id.spinner_cursos) ;
 
-        //spinner1.setOnItemClickListener(this);
-        listaEstdiante = new ArrayList<>();
-        strStudianes = new String[]{ "A/1", "B/2", "C/3"};
-        Collections.addAll(listaEstdiante, strStudianes);
-        comboAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listaEstdiante);
+        //spinner1.setOnItemClickListener(this::onItemSelected);
+        listaCursos = new ArrayList<>();
+        strCursos = new String[]{ "A/1", "B/2", "C/3"};
+        Collections.addAll(listaCursos, strCursos);
+        comboAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listaCursos);
         spinner1.setAdapter(comboAdapter);
+
 
 
         boton1 = findViewById(R.id.button);
@@ -44,10 +47,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), ListaEstudiantes.class);
-                startActivityForResult(intent, 0);
+                intent.putExtra("parametro", nombreCurso);
+                startActivity(intent);
             }
         });
     }
 
-    
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()){
+            case R.id.spinner_cursos:
+                //Almaceno el nombre de la fruta seleccionada
+                nombreCurso = strCursos[position];
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
